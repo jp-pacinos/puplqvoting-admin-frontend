3181048962b451545c21281080e322248fb67668
Jhon Peter <jp.pacinos@gmail.com>
2021-01-19 01:07:56 +0800

    Initial commit

--- src/features/elections/electionSlice.ts
+++ src/features/elections/electionSlice.ts
@@ -2,7 +2,7 @@ import { combineReducers } from '@reduxjs/toolkit'
 import { reducer as electionReducer } from './_id/slice'
 import { reducer as electionsReducer } from './index/slice'
 import { reducer as electionsSettingsReducer } from './_id_settings/slice'
-import { reducer as electionKeysReducer } from './_id_settings'
+import { reducer as electionKeysReducer } from './_id_keys/slice'
 
 const reducers = combineReducers({
   indexPage: electionsReducer,
