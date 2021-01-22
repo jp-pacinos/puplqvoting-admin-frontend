10ef01be5d59607493b99d51d3a254f042191387
Jhon Peter <jp.pacinos@gmail.com>
2021-01-22 19:24:42 +0800

    update Route.tsx
    update Router.tsx

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 // layouts
-import { RouteWithLayout } from 'common/components'
 import { Main as MainLayout } from 'app/layouts'
 
 // pages
+import { LoginPage } from 'features/login'
 import {
   ElectionsPage,
   ElectionPage,
@@ -17,37 +18,61 @@ import { StudentsPage } from 'features/students'
 import { CoursesPage } from 'features/courses'
 import { NotFoundPage } from 'features/404'
 
+import { Snackbar } from 'features/snackbar'
+import { selectIsAuth } from 'features/app/appSlice'
+import useFetchInitialData from './hooks/useFetchInitialData'
+import useResponseInterceptor from './hooks/useResponseInterceptor'
+
 const Router: React.FC<{}> = () => {
-  return (
-    <Switch>
-      <RouteWithLayout path="/" layout={MainLayout} component={ElectionsPage} exact />
-
-      <RouteWithLayout path="/elections" layout={MainLayout} component={ElectionsPage} exact />
-      <RouteWithLayout path="/elections/:id" layout={MainLayout} component={ElectionPage} exact />
-      <RouteWithLayout
-        path="/elections/:id/settings"
-        layout={MainLayout}
-        component={ElectionSettingsPage}
-        exact
-      />
-      <RouteWithLayout
-        path="/elections/:id/keys"
-        layout={MainLayout}
-        component={ElectionKeysPage}
-        exact
-      />
-
-      <RouteWithLayout path="/parties" layout={MainLayout} component={PartiesPage} exact />
-      <RouteWithLayout path="/parties/:id" layout={MainLayout} component={PartiesPage_Id} exact />
-
-      <RouteWithLayout path="/students" layout={MainLayout} component={StudentsPage} exact />
-      <RouteWithLayout path="/students/courses" layout={MainLayout} component={CoursesPage} exact />
-
-      <Route to="/404-not-found" component={NotFoundPage} exact />
-
-      <Redirect to="*" from="/404-not-found" />
-    </Switch>
-  )
+  const isAuth = useSelector(selectIsAuth)
+  return <>{isAuth ? <RouteAuth /> : <RouteGuest />}</>
 }
 
 export default Router
+
+//
+
+const RouteGuest: React.FC = () => {
+  return (
+    <>
+      <Switch>
+        <Route path="/login" component={LoginPage} exact />
+        <Redirect to="/login" from="*" />
+      </Switch>
+    </>
+  )
+}
+
+const RouteAuth: React.FC = () => {
+  useResponseInterceptor()
+  useFetchInitialData()
+
+  return (
+    <MainLayout>
+      <Switch>
+        <Route path="/" component={ElectionsPage} exact />
+
+        <Route path="/elections" component={ElectionsPage} exact />
+        <Route path="/elections/:id" component={ElectionPage} exact />
+        <Route
+          path="/elections/:id/settings"
+          layout={MainLayout}
+          component={ElectionSettingsPage}
+          exact
+        />
+        <Route path="/elections/:id/keys" component={ElectionKeysPage} exact />
+
+        <Route path="/parties" component={PartiesPage} exact />
+        <Route path="/parties/:id" component={PartiesPage_Id} exact />
+
+        <Route path="/students" component={StudentsPage} exact />
+        <Route path="/students/courses" component={CoursesPage} exact />
+
+        <Route path="/404-not-found" component={NotFoundPage} exact />
+        <Redirect to="/404-not-found" from="*" />
+      </Switch>
+
+      <Snackbar />
+    </MainLayout>
+  )
+}
