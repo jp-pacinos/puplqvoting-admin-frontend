9e96cef76042bbf9093e33764e3b907a256f26ed
Jhon Peter <jp.pacinos@gmail.com>
2021-01-25 21:45:29 +0800

    added .env.example

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-export const baseURL = 'http://127.0.0.1:8000'
-// export const baseURL = 'http://192.168.1.113:8000'
+export const baseURL = process.env.REACT_APP_API_BASE_URL || '/'
+export const prefix = process.env.REACT_APP_API_PREFIX || ''
 
 const apiClient = axios.create({
-  baseURL: `${baseURL}/api`,
+  baseURL: `${baseURL}${prefix.indexOf('/') === 0 ? prefix : `/${prefix}`}`,
   withCredentials: true,
 })
 
